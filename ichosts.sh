55fb1b3b1cd97e6b162b4900b903d3e40a87d78a
smelendy <smelendy@umw.edu>
2019-02-14 14:56:36 -0500

    Add files via upload

---
+++ ichosts.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# ichhosts.sh 
+# By Shaun Melendy smelendy@umw.edu
+# Purpose creates formated Icinga hosts
+
+exec<$1
+while read line
+do
+hostip=`echo $line | awk {'print $1'}`
+hostname=`echo $line | awk {'print $2'}`
+hostos=`echo $line | awk {'print $3'}`
+hosttemplate="camera-host"
+hosttype="camera"
+
+
+case $hostos in
+Microsoft) hosttemplate="windows-host" ;;
+Red) hosttemplate="linux-host" ;;
+CentOS) hosttemplate="linux-host" ;;
+Ubuntu) hosttemplate="linux-host" ;;
+esac
+
+
+echo "$hostname" 
+
+echo "object Host \"$hostname\" { " >> $1.conf
+echo "import  \"$hosttemplate\" " >> $1.conf 
+echo "address = \"$hostip\" " >> $1.conf 
+echo "vars.type = \"$hosttype\" " >> $1.conf
+echo "}" >>  $1.conf
+echo ""  >> $1.conf
+
+done
+exit 0
